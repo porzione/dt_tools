5ad3b3fda03171c0a073e7298c352a0de2e9e403
Oleg Kravchenko <okravc@gmail.com>
2018-12-03 10:34:39 +0300

    bit faster

--- models.rb
+++ models.rb
@@ -1,30 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+SEQUEL_NO_ASSOCIATIONS = true
+
 require 'sequel'
 
 DB_BASE_PATH = "#{ENV['DT_HOME'] || ENV['HOME']}/.config/darktable"
-DB_LIB  = Sequel.connect "sqlite://#{DB_BASE_PATH}/library.db"
-DB_DATA = Sequel.connect "sqlite://#{DB_BASE_PATH}/data.db"
+DB = Sequel.connect("sqlite://#{DB_BASE_PATH}/library.db").freeze
+DB_DATA = Sequel.connect("sqlite://#{DB_BASE_PATH}/data.db").freeze
 
-CACHE = nil
-Sequel::Model.plugin :caching, CACHE
-
-## main lib
-class Image < Sequel::Model
-  GEO = %i[longitude latitude altitude].freeze
-end
-
-# id folder
-class FilmRoll < Sequel::Model; end
-
-# id name
-class UsedTag < Sequel::Model; end
-
-# imgid tagid
-class TaggedImage < Sequel::Model; end
-
-class MetaData < Sequel::Model(:meta_data)
+module DT
   CREATOR   = 0
   PUBLISHER = 1
   TITLE     = 2
@@ -38,6 +23,18 @@ class MetaData < Sequel::Model(:meta_data)
     DESCR     => 'description',
     RIGHTS    => 'rights'
   }.freeze
+
+  GEO = %i[longitude latitude altitude].freeze
 end
+
+class Image < Sequel::Model; end
+
+class FilmRoll < Sequel::Model; end
+
+class UsedTag < Sequel::Model; end
+
+class TaggedImage < Sequel::Model; end
+
+class MetaData < Sequel::Model(:meta_data); end
 
 class Tag < Sequel::Model(DB_DATA); end
